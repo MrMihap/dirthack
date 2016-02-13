f7420e70fb4e37a0ddf7192dcdc11e0948a94a41
asveredyuk <alexsv1802@gmail.com>
2016-02-13 15:09:10 +0400

    Check added

--- main/main/Form1.Designer.cs
+++ main/main/Form1.Designer.cs
@@ -28,13 +28,25 @@
     /// </summary>
     private void InitializeComponent()
     {
+            this.btCheck = new System.Windows.Forms.Button();
             this.SuspendLayout();
+            // 
+            // btCheck
+            // 
+            this.btCheck.Location = new System.Drawing.Point(569, 12);
+            this.btCheck.Name = "btCheck";
+            this.btCheck.Size = new System.Drawing.Size(75, 23);
+            this.btCheck.TabIndex = 0;
+            this.btCheck.Text = "Check!";
+            this.btCheck.UseVisualStyleBackColor = true;
+            this.btCheck.Click += new System.EventHandler(this.btCheck_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(656, 329);
+            this.Controls.Add(this.btCheck);
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
@@ -43,6 +55,8 @@
     }
 
     #endregion
+
+    private System.Windows.Forms.Button btCheck;
   }
 }
 
--- main/main/Form1.cs
+++ main/main/Form1.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Diagnostics;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -12,6 +13,7 @@ namespace main
 {
   public partial class Form1 : Form
   {
+      const string SOLNAME = "solution.txt";
     public Form1()
     {
       InitializeComponent();
@@ -20,7 +22,26 @@ namespace main
     private void Form1_Load(object sender, EventArgs e)
     {
         DataContainer.Load("times.txt", "order.csv");
-        MessageBox.Show(string.Format("{0},{1}", DataContainer.routes.Length, DataContainer.orders.Count));
+        //MessageBox.Show(string.Format("{0},{1}", DataContainer.routes.Length, DataContainer.orders.Count));
+    }
+
+    private void btCheck_Click(object sender, EventArgs e)
+    {
+        string checkResult = RunChecker();
+        MessageBox.Show(checkResult);
+    }
+    private string RunChecker()
+    {
+        string procName = "python";
+        string args = " check.py " + SOLNAME;
+        Process p = new Process();
+        p.StartInfo.FileName = procName;
+        p.StartInfo.Arguments = args;
+        p.StartInfo.UseShellExecute = false;
+        p.StartInfo.RedirectStandardOutput = true;
+        p.Start();
+        string output = p.StandardOutput.ReadToEnd();
+        return output;
     }
   }
 }
